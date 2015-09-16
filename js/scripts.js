07ae5e0893c160e6850e2f2ad0e479c1f4433a95
Alex Altieri & Lindsay Culver <student@epicodus.com>
2015-09-16 11:24:31 -0700

    set up spec for winning plays

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,48 @@ function Player(mark) {
 function Space(xCoordinate, yCoordinate) {
   this.xCoordinate = xCoordinate;
   this.yCoordinate = yCoordinate;
+
   this.markedBy = null;
 }
 
 Space.prototype.markBy = function(player) {
   this.markedBy = player;
-}
+};
 
 function Board(spaces) {
   this.spaces = [(1,1), (1,2), (1,3), (2,1), (2,2), (2,3), (3,1), (3,2), (3,3)];
-  (1,1), (1,2), (1,3) #across the top
-  (2,1), (2,2), (2,3)#across the middle
-  (3,1), (3,2), (3,3) #across the botton
-
-
-  (1,1), (2,2), (3,3) #diagonal left to right
-  (3,1), (2,2), (1,3)#diagonal right to left
-
-  (1,3), (2,3), (3,3)#down the right
-  (1,1), (2,1), (3,1)#down the left
-  (1,2). (2,2), (3,2)#down the middle
 
 }
+
+Board.prototype.win = function() {
+  var winningCombos = [
+    [(1,1), (1,2), (1,3)], [(2,1), (2,2), (2,3)], [(3,1), (3,2), (3,3)],
+    [(1,1), (2,2), (3,3)], [(3,1), (2,2), (1,3)],
+    [(1,3), (2,3), (3,3)], [(1,1), (2,1), (3,1)], [(1,2), (2,2), (3,2)]
+  ];
+
+  winningCombos.forEach(function(combo){
+
+  });
+};
+
+// -------original attempted loop
+// if ((combo[0].markedBy === combo[1].markedBy) && (combo[0].markedBy === combo[2].markedBy) && (combo[1].markedBy === combo[2].markedBy)) {
+//   return true;
+// } else {
+//   return false;
+// }
+
+
+
+// (1,1), (1,2), (1,3) #across the top
+// (2,1), (2,2), (2,3)#across the middle
+// (3,1), (3,2), (3,3) #across the bottom
+//
+//
+// (1,1), (2,2), (3,3) #diagonal left to right
+// (3,1), (2,2), (1,3)#diagonal right to left
+//
+// (1,3), (2,3), (3,3)#down the right
+// (1,1), (2,1), (3,1)#down the left
+// (1,2), (2,2), (3,2)#down the middle
--- spec/specs.js
+++ spec/specs.js
@@ -19,7 +19,7 @@ describe('Space', function() {
   it("lets a player mark a space", function() {
     var testPlayer = new Player("X");
     var testSpace = new Space(1,2);
-    testSpace.markBy(testPlayer)
+    testSpace.markBy(testPlayer);
     expect(testSpace.markedBy).to.equal(testPlayer);
   });
 });
@@ -29,5 +29,18 @@ describe("Board", function() {
   it("creates 9 spaces when it is initialized", function(){
     var testBoard = new Board([(1,1), (1,2), (1,3), (2,1), (2,2), (2,3), (3,1), (3,2), (3,3)]);
     expect(testBoard.spaces).to.eql([(1,1), (1,2), (1,3), (2,1), (2,2), (2,3), (3,1), (3,2), (3,3)]);
+  });
+
+  it("tells if there are 3 spaces in a row marked by the same player", function() {
+    var testPlayer = new Player("X");
+    var testBoard = new Board([(1,1), (1,2), (1,3), (2,1), (2,2), (2,3), (3,1), (3,2), (3,3)]);
+    var testSpace1 = new Space(1,1);
+    var testSpace2 = new Space(1,2);
+    var testSpace3 = new Space(1,3);
+    testSpace1.markBy(testPlayer);
+    testSpace2.markBy(testPlayer);
+    testSpace3.markBy(testPlayer);
+    debugger;
+    expect(testBoard.win()).to.equal(true);
   });
 });
